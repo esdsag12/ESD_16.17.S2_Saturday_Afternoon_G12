5184bb84bf01bb357fe42cc033ea7310ffbcc6f1
CuongNV <cuongdollar2995@gmail.com>
2017-05-07 18:09:50 +0700

    [CuongNV] update  doc am vi

--- ESD/app/src/main/java/com/app/esd/esd/Activity/MainActivity.java
+++ ESD/app/src/main/java/com/app/esd/esd/Activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.app.esd.esd.Activity;
 
 import android.app.Dialog;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,12 +16,14 @@ import com.app.esd.esd.Modals.ApiModals.OxfordObject;
 import com.app.esd.esd.Modals.ServicesModals.OxfordPronunciationService;
 import com.app.esd.esd.R;
 
-public class MainActivity extends BaseActivity implements OxfordPronuncationListener, View.OnClickListener {
-    Button btnDich;
-    EditText edtText;
+import java.io.IOException;
+
+public class MainActivity extends BaseActivity implements OxfordPronuncationListener, View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+    Button btnDich, btnDoc;
+    EditText edtText, edtSo;
     String text = "";
     int length = 0, totalLength = 0;
-
+    MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,11 @@ public class MainActivity extends BaseActivity implements OxfordPronuncationList
         setContentView(R.layout.activity_main);
         btnDich = (Button) findViewById(R.id.btn_dich);
         edtText = (EditText) findViewById(R.id.edt_text);
+        btnDoc = (Button) findViewById(R.id.btn_doc);
+        edtSo = (EditText) findViewById(R.id.edt_so);
+        btnDoc.setOnClickListener(this);
         btnDich.setOnClickListener(this);
+
 
     }
 
@@ -56,7 +65,39 @@ public class MainActivity extends BaseActivity implements OxfordPronuncationList
                 for (int i = 0; i < words.length; i++) {
                     new OxfordPronunciationService(this).execute(words[i].trim());
                 }
+
                 break;
+            case R.id.btn_doc:
+                if (Integer.parseInt(edtSo.getText().toString()) < 45 && Integer.parseInt(edtSo.getText().toString())>0) {
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.setOnErrorListener(this);
+                    try {
+                        mediaPlayer.setDataSource(getApplicationContext(), Uri.parse("https://noidung.tienganh123.com/file/baihoc/pronunciation/coban/" +
+                                "bai" + edtSo.getText().toString().trim() + "/u" + edtSo.getText().toString().trim() + ".mp3"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    mediaPlayer.prepareAsync();
+                    mediaPlayer.setOnCompletionListener(this);
+                }
+                break;
+
         }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
     }
 }

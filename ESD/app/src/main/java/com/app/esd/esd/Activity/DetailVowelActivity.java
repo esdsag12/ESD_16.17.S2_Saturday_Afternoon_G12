86b42c2affa0a0d75564f99aeec7983d87831121
dobynt <nguyentranbaoduy95@gmail.com>
2017-05-10 12:17:43 +0700

    Duy Nguyễn: Merge code Leaning Vowel ver 2

--- ESD/app/src/main/java/com/app/esd/esd/Activity/DetailVowelActivity.java
+++ ESD/app/src/main/java/com/app/esd/esd/Activity/DetailVowelActivity.java
@@ -15,13 +15,16 @@ import com.app.esd.esd.R;
 
 import java.io.IOException;
 
-public class DetailVowelActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener{
+public class DetailVowelActivity extends AppCompatActivity implements View.OnClickListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
     private TextView tvContent;
     private String vowel;
     private Toolbar toolbar;
-    private ImageView imgSpeak,img;
+    private ImageView imgSpeak, img;
     private MediaPlayer mediaPlayer = new MediaPlayer();
     private int id;
+
+    private String[] list1,list2,list3,list4,list5,list6,list7,list8,list9,list10,list11,list12,
+            list13,list14,list15,list16,list17,list18,list19,list20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +32,7 @@ public class DetailVowelActivity extends AppCompatActivity implements View.OnCli
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             vowel = extras.getString("vowel");
-            id=extras.getInt("id");
+            id = extras.getInt("id");
         }
         init();
         setToolbar();
@@ -40,8 +43,8 @@ public class DetailVowelActivity extends AppCompatActivity implements View.OnCli
     public void init() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         tvContent = (TextView) findViewById(R.id.tvContent);
-        imgSpeak = (ImageView)findViewById(R.id.imgSpeak);
-        img= (ImageView)findViewById(R.id.img);
+        imgSpeak = (ImageView) findViewById(R.id.imgSpeak);
+        img = (ImageView) findViewById(R.id.img);
     }
 
     public void setToolbar() {
@@ -51,74 +54,75 @@ public class DetailVowelActivity extends AppCompatActivity implements View.OnCli
         getSupportActionBar().setTitle(vowel);
     }
 
-    public void setEventClick()
-    {
+    public void setEventClick() {
         imgSpeak.setOnClickListener(this);
     }
 
-    public void setData(int i)
-    {
-        if(i==1)
-        {
+    public void setData(int i) {
+        if (i == 1) {
             tvContent.setText(getResources().getString(R.string.vowel1));
             img.setImageResource(R.drawable.vowel1);
-        }
-        else if (i==2)
-        {
+        } else if (i == 2) {
             tvContent.setText(getResources().getString(R.string.vowel2));
             img.setImageResource(R.drawable.vowel2);
-        }
-        else if (i==3)
-        {
+        } else if (i == 3) {
             tvContent.setText(getResources().getString(R.string.vowel3));
             img.setImageResource(R.drawable.vowel3);
-        }
-        else if (i==4)
-        {
+        } else if (i == 4) {
             tvContent.setText(getResources().getString(R.string.vowel4));
             img.setImageResource(R.drawable.vowel4);
-        }
-        else if (i==5)
-        {
+        } else if (i == 5) {
             tvContent.setText(getResources().getString(R.string.vowel5));
             img.setImageResource(R.drawable.vowel5);
-        }
-        else if (i==6)
-        {
+        } else if (i == 6) {
             tvContent.setText(getResources().getString(R.string.vowel6));
             img.setImageResource(R.drawable.vowel6);
-        }
-        else if (i==7)
-        {
+        } else if (i == 7) {
             tvContent.setText(getResources().getString(R.string.vowel7));
             img.setImageResource(R.drawable.vowel7);
-        }
-        else if (i==8)
-        {
+        } else if (i == 8) {
             tvContent.setText(getResources().getString(R.string.vowel8));
             img.setImageResource(R.drawable.vowel8);
-        }
-        else if (i==9)
-        {
+        } else if (i == 9) {
             tvContent.setText(getResources().getString(R.string.vowel9));
             img.setImageResource(R.drawable.vowel9);
-        }
-        else if (i==10)
-        {
+        } else if (i == 10) {
             tvContent.setText(getResources().getString(R.string.vowel10));
             img.setImageResource(R.drawable.vowel10);
-        }
-        else if (i==11)
-        {
+        } else if (i == 11) {
             tvContent.setText(getResources().getString(R.string.vowel11));
             img.setImageResource(R.drawable.vowel11);
-        }
-        else if (i==12)
-        {
+        } else if (i == 12) {
             tvContent.setText(getResources().getString(R.string.vowel12));
             img.setImageResource(R.drawable.vowel12);
+        }else if (i == 13) {
+            tvContent.setText(getResources().getString(R.string.vowel13));
+            img.setImageResource(R.drawable.vowel13);
+        }else if (i == 14) {
+            tvContent.setText(getResources().getString(R.string.vowel14));
+            img.setImageResource(R.drawable.vowel14);
+        }else if (i == 15) {
+            tvContent.setText(getResources().getString(R.string.vowel15));
+            img.setImageResource(R.drawable.vowel15);
+        }else if (i == 16) {
+            tvContent.setText(getResources().getString(R.string.vowel16));
+            img.setImageResource(R.drawable.vowel16);
+        }else if (i == 17) {
+            tvContent.setText(getResources().getString(R.string.vowel17));
+            img.setImageResource(R.drawable.vowel17);
+        }else if (i == 18) {
+            tvContent.setText(getResources().getString(R.string.vowel18));
+            img.setImageResource(R.drawable.vowel18);
+        }else if (i == 19) {
+            tvContent.setText(getResources().getString(R.string.vowel19));
+            img.setImageResource(R.drawable.vowel19);
+        }else if (i == 20) {
+            tvContent.setText(getResources().getString(R.string.vowel20));
+            img.setImageResource(R.drawable.vowel20);
         }
+
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -127,8 +131,7 @@ public class DetailVowelActivity extends AppCompatActivity implements View.OnCli
         return super.onOptionsItemSelected(item);
     }
 
-    public void speakVowel(int i)
-    {
+    public void speakVowel(int i) {
         if (i < 45 && i > 0) {
             mediaPlayer = new MediaPlayer();
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -144,6 +147,7 @@ public class DetailVowelActivity extends AppCompatActivity implements View.OnCli
             mediaPlayer.setOnCompletionListener(this);
         }
     }
+
     @Override
     public void onPrepared(MediaPlayer mp) {
         mp.start();
@@ -160,7 +164,7 @@ public class DetailVowelActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.imgSpeak:
                 speakVowel(id);
         }

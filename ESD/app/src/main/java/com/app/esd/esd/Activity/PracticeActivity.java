5a3abf2d91cc21a07b649b96f4373ec566741b81
tungpham6195 <phamthanhtung1995@gmail.com>
2017-05-13 22:41:37 +0700

    [TUNG] design main, learnging, practice

--- ESD/app/src/main/java/com/app/esd/esd/Activity/PracticeActivity.java
+++ ESD/app/src/main/java/com/app/esd/esd/Activity/PracticeActivity.java
@@ -1,12 +1,24 @@
 package com.app.esd.esd.Activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.app.esd.esd.Adapter.PractVPAdapter;
 import com.app.esd.esd.R;
 
 public class PracticeActivity extends BaseActivity {
+    ViewPager viewPager;
+    View view_readpract, view_chooseword, view_keyboard;
+    Button btn_read;
+    PractVPAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,26 @@ public class PracticeActivity extends BaseActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_practice);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_pract);
+        pagerAdapter = new PractVPAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setClipToPadding(false);
+        viewPager.setPadding((int) dipToPixels(this, 32), 0, (int) dipToPixels(this, 32), 0);
+        viewPager.setPageMargin((int) dipToPixels(this, 32));
+        viewPager.setOffscreenPageLimit(pagerAdapter.getCount());
+        pagerAdapter.setOnBtnItemClickListener(new PractVPAdapter.OnBtnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(PracticeActivity.this, position + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        viewPager.setPageMargin(300);
+//        viewPager.addView(, params);
+//        viewPager.addView(view_keyboard, params);
+    }
+
+    public float dipToPixels(Context context, float dipValue) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dipValue, metrics);
     }
 }
---
+++ ESD/app/src/main/java/com/app/esd/esd/Adapter/PractVPAdapter.java
@@ -0,0 +1,126 @@
+package com.app.esd.esd.Adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.app.esd.esd.R;
+
+/**
+ * Created by PTT on 5/13/2017.
+ */
+
+public class PractVPAdapter extends PagerAdapter {
+    Context context;
+    OnBtnItemClickListener onBtnItemClickListener;
+
+    public void setOnBtnItemClickListener(
+            PractVPAdapter.OnBtnItemClickListener onBtnItemClickListener) {
+        this.onBtnItemClickListener = onBtnItemClickListener;
+    }
+
+    public interface OnBtnItemClickListener {
+        void onClick(int position);
+    }
+
+    public PractVPAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return CustomPagerEnum.values().length;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+        LayoutInflater inflater = LayoutInflater.from(context);
+        ViewGroup layout = (ViewGroup) inflater.inflate(customPagerEnum.getLayoutResId(),
+                container, false);
+        container.addView(layout);
+        TextView text_des;
+        Button btn_open;
+        switch (position) {
+            case 0:
+                text_des = (TextView) layout.findViewById(R.id.tv_keyboard_pager);
+                btn_open = (Button) layout.findViewById(R.id.btn_openKeyboard_pager);
+                btn_open.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onBtnItemClickListener.onClick(position);
+                    }
+                });
+                text_des.setText(customPagerEnum.getTitle());
+                break;
+            case 1:
+                text_des = (TextView) layout.findViewById(R.id.tv_speak_pager);
+                text_des.setText(customPagerEnum.getTitle());
+                btn_open = (Button) layout.findViewById(R.id.btn_openSpeak_pager);
+                btn_open.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onBtnItemClickListener.onClick(position);
+                    }
+                });
+                break;
+            case 2:
+                text_des = (TextView) layout.findViewById(R.id.tv_choose_pager);
+                text_des.setText(customPagerEnum.getTitle());
+                btn_open = (Button) layout.findViewById(R.id.btn_openChoose_pager);
+                btn_open.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onBtnItemClickListener.onClick(position);
+                    }
+                });
+                break;
+        }
+        return layout;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object view) {
+        container.removeView((View) view);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public float getPageWidth(int position) {
+        return 0.8f;
+    }
+
+    public enum CustomPagerEnum {
+
+        KEYBOARD("keyboard", R.layout.layout_pager_keyboard),
+        SPEAKING("speaking", R.layout.layout_pager_speak),
+        CHOOSE("quick test", R.layout.layout_pager_choose);
+//        BLUE(R.layout.view_blue),
+//        ORANGE(R.layout.view_orange);
+
+        private int mLayoutResId;
+        private String title;
+
+        CustomPagerEnum(String title, int layoutResId) {
+            this.title = title;
+            mLayoutResId = layoutResId;
+        }
+
+        public int getLayoutResId() {
+            return mLayoutResId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+}
